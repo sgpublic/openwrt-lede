bb97ce6705c75ba62181a9de3940242170767a70
sgpublic <3300633844@qq.com>
2022-04-12 19:20:50 +0800

    remove unnecessary plugins, bug fix, add sftp support.

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,15 +14,10 @@ if [ "$1" == "--local" ]; then
   set -v
 fi
 
-rm -f feeds.conf
-cp feeds.conf.default feeds.conf
-
 # 添加软件源 fw876/helloworld
-sed -i "/helloworld/d" feeds.conf.default
-echo 'src-git-full helloworld https://github.com/fw876/helloworld.git' >> feeds.conf
+git clone https://github.com/fw876/helloworld.git package/helloworld
 # 添加软件源 sundaqiang/openwrt-packages
-sed -i "/sundaqiang/d" feeds.conf.default
-echo 'src-git-full sundaqiang https://github.com/sgpublic/openwrt-packages-sundaqiang.git' >> feeds.conf
+git clone https://github.com/sundaqiang/openwrt-packages.git package/sundaqiang
 
 if [ "$1" == "--local" ]; then
   # 本地拉取依赖
@@ -43,15 +38,6 @@ svn co https://github.com/jerrykuku/luci-theme-argon/trunk package/jerrykuku/luc
 rm -rf package/jerrykuku/luci-app-argon-config
 svn co https://github.com/jerrykuku/luci-app-argon-config/trunk package/jerrykuku/luci-app-argon-config
 
-# 拉取插件 luci-app-arpbind
-rm -rf package/lean/luci-app-arpbind
-svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-arpbind package/lean/luci-app-arpbind
-sed -i "s/include ..\/..\/luci.mk/include \$(TOPDIR)\/feeds\/luci\/luci.mk/g" package/lean/luci-app-arpbind/Makefile
-
 # 拉取插件 luci-app-filebrowser
-rm -rf package/xiaozhuai/luci-app-filebrowser
-svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/Lienol/luci-app-filebrowser
-
-# 拉取插件 luci-app-openclash
-rm -rf package/vernesong/luci-app-openclash
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/vernesong/luci-app-openclash
+rm -rf package/sgpublic/luci-app-filebrowser
+svn co https://github.com/sgpublic/luci-app-filebrowser/trunk package/sgpublic/luci-app-filebrowser
